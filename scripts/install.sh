ed08dc15ea97045a08eb2671c2fe928de0194d70
WingLim <643089849@qq.com>
2021-05-16 01:25:20 +0800

    feat: add uninstall action

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,9 +9,11 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 # Clean up old installs
-function cleanUpOldInstall() {
+function uninstall_MicFix() {
+    echo "Removing old installs"
     sudo launchctl unload /Library/LaunchAgents/com.WingLim.MicFix.plist
     sudo rm -rf /Library/LaunchAgents/com.WingLim.MicFix.plist
+    echo "Reomve successful!"
 }
 
 # Function that exits with an error code and message
@@ -20,25 +22,35 @@ function abort() {
     exit 1
 }
 
-# Get root permission
-if [[ $EUID -ne 0 ]];
-then
-    exec sudo /bin/bash "$0" "$@"
-fi
+function install_MicFix() {
+    uninstall_MicFix 2>/dev/null
 
-echo "Removing old installs"
-cleanUpOldInstall 2>/dev/null
+    echo "Copying new files"
+    sudo cp "$DIR/MicFix" /usr/local/bin/MicFix || abort "Failed to copy MicFix"
+    sudo cp "$DIR/com.WingLim.MicFix.plist" /Library/LaunchAgents || abort "Failed to copy launchd plist file"
 
-echo "Copying new files"
-sudo cp "$DIR/MicFix" /usr/local/bin/MicFix || abort "Failed to copy MicFix"
-sudo cp "$DIR/com.WingLim.MicFix.plist" /Library/LaunchAgents || abort "Failed to copy launchd plist file"
+    echo "Setting permissions"
+    sudo chmod 755 /usr/local/bin/MicFix
+    sudo chmod 644 /Library/LaunchAgents/com.WingLim.MicFix.plist
 
-echo "Setting permissions"
-sudo chmod 755 /usr/local/bin/MicFix
-sudo chmod 644 /Library/LaunchAgents/com.WingLim.MicFix.plist
+    sudo chown root:wheel /usr/local/bin/MicFix
+    sudo chown root:wheel /Library/LaunchAgents/com.WingLim.MicFix.plist
 
-sudo chown root:wheel /usr/local/bin/MicFix
-sudo chown root:wheel /Library/LaunchAgents/com.WingLim.MicFix.plist
+    echo "Loading launch daemon"
+    sudo launchctl load /Library/LaunchAgents/com.WingLim.MicFix.plist
+    
+    echo "Install successful!"
+}
 
-echo "Loading launch daemon"
-sudo launchctl load /Library/LaunchAgents/com.WingLim.MicFix.plist
+
+action=$1
+[ -z "$1" ] && action=install
+case "$action" in
+    install|uninstall)
+        ${action}_MicFix
+        ;;
+    *)
+        echo "Wrong arguments [${action}]"
+        echo "Usage: $(basename "$0") [install|uninstall]"
+        ;;
+esac
