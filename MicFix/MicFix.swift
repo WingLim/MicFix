af1137e4bf074beaef5c40f5136a17d54d2c6ed9
WingLim <643089849@qq.com>
2021-05-16 15:51:18 +0800

    reafactor: add alc prefix to coef function

--- MicFix/MicFix.swift
+++ MicFix/MicFix.swift
@@ -157,11 +157,11 @@ class MicFix {
         }
     }
     
-    private func readCoef(idx: UInt32) -> Int32 {
-        return readCoefEX(nid: REALTEK_VENDOR_REGISTERS, idx: idx)
+    private func alcReadCoef(idx: UInt32) -> Int32 {
+        return alcReadCoefEX(nid: REALTEK_VENDOR_REGISTERS, idx: idx)
     }
     
-    private func readCoefEX(nid: UInt32, idx: UInt32) -> Int32 {
+    private func alcReadCoefEX(nid: UInt32, idx: UInt32) -> Int32 {
         var val: Int32 = 0
         
         _ = sendHdaVerb(HDAVerb(nid: nid, verb: SET_COEF_INDEX, param: idx))
@@ -170,25 +170,25 @@ class MicFix {
         return val
     }
     
-    private func writeCoef(idx: UInt32, value: UInt32) {
-        writeCoefEX(nid: REALTEK_VENDOR_REGISTERS, idx: idx, value: value)
+    private func alcWriteCoef(idx: UInt32, value: UInt32) {
+        alcWriteCoefEX(nid: REALTEK_VENDOR_REGISTERS, idx: idx, value: value)
     }
     
-    private func writeCoefEX(nid: UInt32, idx: UInt32, value: UInt32) {
+    private func alcWriteCoefEX(nid: UInt32, idx: UInt32, value: UInt32) {
         _ = sendHdaVerb(HDAVerb(nid: nid, verb: SET_COEF_INDEX, param: idx))
         _ = sendHdaVerb(HDAVerb(nid: nid, verb: SET_PROC_COEF, param: value))
     }
     
-    private func updateCoef(idx: UInt32, mask: Int32, value: UInt32) {
-        updateCoefEX(nid: REALTEK_VENDOR_REGISTERS, index: idx, mask: mask, value: value)
+    private func alcUpdateCoef(idx: UInt32, mask: Int32, value: UInt32) {
+        alcUpdateCoefEX(nid: REALTEK_VENDOR_REGISTERS, index: idx, mask: mask, value: value)
     }
     
-    private func updateCoefEX(nid: UInt32, index: UInt32, mask: Int32, value: UInt32) {
-        let val = readCoefEX(nid: nid, idx: index)
+    private func alcUpdateCoefEX(nid: UInt32, index: UInt32, mask: Int32, value: UInt32) {
+        let val = alcReadCoefEX(nid: nid, idx: index)
         let tmp = Int32(value)
         
         if val != -1 {
-            writeCoefEX(nid: nid, idx: index, value: UInt32((val & ~mask) | tmp))
+            alcWriteCoefEX(nid: nid, idx: index, value: UInt32((val & ~mask) | tmp))
         }
     }
     
@@ -199,9 +199,9 @@ class MicFix {
         switch codecID {
         case ALC255:
             // Comes from https://github.com/torvalds/linux/blob/63d1cb53e26a9a4168b84a8981b225c0a9cfa235/sound/pci/hda/patch_realtek.c#L5026
-            writeCoef(idx: 0x45, value: 0xd489)
-            writeCoef(idx: 0x1b, value: 0x0c2b)
-            writeCoefEX(nid: 0x57, idx: 0x03, value: 0x8ea6)
+            alcWriteCoef(idx: 0x45, value: 0xd489)
+            alcWriteCoef(idx: 0x1b, value: 0x0c2b)
+            alcWriteCoefEX(nid: 0x57, idx: 0x03, value: 0x8ea6)
         default: break
         }
     }
@@ -213,9 +213,9 @@ class MicFix {
         switch codecID {
         case ALC255:
             // Comes from https://github.com/torvalds/linux/blob/63d1cb53e26a9a4168b84a8981b225c0a9cfa235/sound/pci/hda/patch_realtek.c#L5144
-            writeCoef(idx: 0x45, value: 0xe489)
-            writeCoef(idx: 0x1b, value: 0x0c2b)
-            writeCoefEX(nid: 0x57, idx: 0x03, value: 0x8ea6)
+            alcWriteCoef(idx: 0x45, value: 0xe489)
+            alcWriteCoef(idx: 0x1b, value: 0x0c2b)
+            alcWriteCoefEX(nid: 0x57, idx: 0x03, value: 0x8ea6)
         default: break
         }
     }
@@ -231,10 +231,10 @@ class MicFix {
         case ALC255:
             _ = sendHdaVerb(HDAVerb(nid: 0x19, verb: SET_PIN_WIDGET_CONTROL, param: 0x24))
             // Comes from https://github.com/torvalds/linux/blob/63d1cb53e26a9a4168b84a8981b225c0a9cfa235/sound/pci/hda/patch_realtek.c#L5245
-            writeCoef(idx: 0x45, value: 0xd089)
-            writeCoef(idx: 0x49, value: 0x0149)
+            alcWriteCoef(idx: 0x45, value: 0xd089)
+            alcWriteCoef(idx: 0x49, value: 0x0149)
             usleep(350000)
-            val = readCoef(idx: 0x46)
+            val = alcReadCoef(idx: 0x46)
             isCTIA = (val & 0x0070) == 0x0070
         default: break
         }
@@ -252,11 +252,11 @@ class MicFix {
         print("Jack Status: unplugged.\n")
         switch codecID {
         case ALC255:
-            writeCoef(idx: 0x1b, value: 0x0c0b)
-            writeCoef(idx: 0x45, value: 0xd089)
-            updateCoefEX(nid: 0x57, index: 0x05, mask: 1<<14, value: 0)
-            writeCoef(idx: 0x06, value: 0x6104)
-            writeCoefEX(nid: 0x57, idx: 0x03, value: 0x8aa6)
+            alcWriteCoef(idx: 0x1b, value: 0x0c0b)
+            alcWriteCoef(idx: 0x45, value: 0xd089)
+            alcUpdateCoefEX(nid: 0x57, index: 0x05, mask: 1<<14, value: 0)
+            alcWriteCoef(idx: 0x06, value: 0x6104)
+            alcWriteCoefEX(nid: 0x57, idx: 0x03, value: 0x8aa6)
         default: break
         }
     }
