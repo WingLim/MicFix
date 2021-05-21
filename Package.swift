40e744932ce9b470538fd26559e95c0aa657f866
WingLim <643089849@qq.com>
2021-05-21 17:58:41 +0800

    refactor: use swift package manager

---
+++ Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version:5.4
+
+import PackageDescription
+
+let package = Package(
+    name: "MicFix",
+    targets: [
+        .executableTarget(
+            name: "MicFix"
+        )
+    ]
+)
